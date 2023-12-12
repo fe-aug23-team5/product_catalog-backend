318181979db03ca36f50810616449437469447da
elenagrasovskaya <elenagrasovskaya@gmail.com>
2023-12-12 16:43:52 +0200

    fixing connection deploy issue

--- src/utils/initDb.ts
+++ src/utils/initDb.ts
@@ -19,7 +19,5 @@ export const connect = async () => {
     console.log('Database connection has been established successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
-  } finally {
-    sequelize.close()
   }
 };
